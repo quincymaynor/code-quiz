fc37c92961ce440b51b94cc336dc16d4442b18dc
Quincy Maynor <131222776+quincymaynor@users.noreply.github.com>
2023-05-25 23:58:46 -0500

    got rid of extra comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,12 +39,8 @@ var question5 = {
     options: [1,2,"Document Object Model",4],
     answer: "Document Object Model",
 }
-var question6 = {
-    text: "blank",
-    options: [1,2,3,4],
-    answer: 1,
-}
-var quizQuestions = [question1, question2, question3, question4, question5, question6]
+
+var quizQuestions = [question1, question2, question3, question4, question5]
 
 var currentQuestion = quizQuestions[0];
 
@@ -80,6 +76,17 @@ function pushQuestion() {
         answerOptions.appendChild(listItem);
     }
 
+    answerOptions.addEventListener("click", function(event) {
+        var userChoice = event.target.classList.contains('answer-btn');
+    
+        if (userChoice == correctAnswer) {
+        quizQuestions++;
+        }else{
+        secondsLeft-10;
+        quizQuestions++;
+        }
+    })
+
 }
   
 //function to end quiz
@@ -103,78 +110,17 @@ function playGame() {
 
     pushQuestion();
 
-    answerOptions.addEventListener("click", function(event) {
-        var userChoice = event.target.classList.contains('answer-btn');
-    
-        if (userChoice == correctAnswer) {
-        quizQuestions++;
-        }else{
-        secondsLeft-10;
-        quizQuestions++;
-        }
-    })
 }
 
 startButton.addEventListener("click", playGame);
 
-
-//start with langing page being visible and the others with display none
-
-//start button listen for click and make landing page diplay none and quiz end display none, start timer
-
-//QUIZ FUNCTION
-
-//display question one at a time
-
-//if a question is answered incorrectly time is deducted
-
-//once time reaches 0 or all questions are answered save the time left and make quiz display none while submission page is displayed
-
-//SCORE SUBMISSION FUNCTION 
-
-//display time left
-
-//input initials
-
-//move to highscores page
-
-/*
-var student = document.getElementById("student-names");
-var grade = document.getElementById("grades");
-var comment = document.getElementById("msg");
-var saveButton = document.getElementById("save");
-var savedName = document.getElementById("saved-name");
-
-saveButton.addEventListener("click", function(event) {
-event.preventDefault();
-
-var studentGrade = {
-  student: student.value,
-  grade: grade.value,
-  comment: comment.value.trim()
-};
-
-localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-renderMessage();
-
-});
-
-function renderMessage() {
-  var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
-  if (lastGrade !== null) {
-    document.querySelector(".message").textContent = lastGrade.student + 
-    " received a/an " + lastGrade.grade
-  }
-}
-*/
-
+//highscores page
 function highscores() {
     var highscoresList = JSON.parse(localStorage.getItem("highscores"));
 
     for (let index = 0; index < array.length; index++) {
         
-        const element = highscoresList[index];
-
+        var element = highscoresList[index];
         var listItem = document.createElement("li");
 
         listItem.textContent = element;
@@ -184,43 +130,3 @@ function highscores() {
         scoresList.appendChild(listItem);
     }
 }
-
-/*
-
-var question = function(text, options, answer) {
-    return {
-        index: 0,
-        text: text,
-        options: options,
-        answer: answer,
-        onAnswer: null
-    }
-}
-
-var quiz = function() {
-    function showQuestion(index) {
-        if (self.question.length <= index) {
-            console.log("all done")
-        }
-        else {
-            questionText.textContent = quizQuestions[index].text;
-            answerOptions.textContent = self.question[index].options;
-        }
-    }
-
-    function doAddQuestion(question) {
-        question.index = self.questions.length;
-        self.questions.push(question);
-        question.onAnswer = function() {
-            showQuestion(question.index + 1)
-        }
-    };
-
-    return {
-        timer: null,
-        questions: [],
-        addQuestion: doAddQuestion
-    }    
-}
-
-*/
